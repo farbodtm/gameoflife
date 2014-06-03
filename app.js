f369fb62e9e93b8dc1963eff60158e3b1b1d212c
Mashiya <farbod.motlagh@gmail.com>
2014-06-03 16:05:38 +0930

    Comments

--- app.js
+++ app.js
@@ -10,7 +10,6 @@
   container.style.textAlign = "center";
   var canvas = document.createElement("canvas");
   canvasSetSize();
-  //window.addEventListener("resize", canvasSetSize, false);
 
   container.appendChild(canvas);
 
@@ -21,13 +20,85 @@
     var context = canvas.getContext('2d');
   }
 
+
   window.addEventListener("click", draw);
   //  window.addEventListener("keyup", function() { setInterval(nextGeneration, 150); });
-  
+ 
   context.fillStyle = deadColor; 
   context.fillRect (0, 0, canvas.width, canvas.height);
 
   
+  // TODO make a cell alive with mouse
+  // Doesn't work properly
+  function draw(e) {
+    context.fillStyle = aliveColor;
+    var x = (Math.ceil(e.clientX/3)*3) + Math.floor(size/2);
+    var y = (Math.ceil(e.clientY/3)*3) + Math.floor(size/2);
+    context.fillStyle = aliveColor;
+    context.fillRect (x, y, 10*size, 10*size);
+  }
+
+
+  // Core Algorithm of generating the next step
+  function nextGeneration() {
+    var width, height, imageData;
+    width = canvas.width;
+    height = canvas.height;
+    imageData = context.getImageData(0, 0, width, height);
+    var numOfAliveNeighbours;
+    for (var i = Math.floor(size/2); i < width; i = i+size){
+      for (var j = Math.floor(size/2); j < height; j = j+size){
+	numOfAliveNeighbours = neighbours(i, j, size, imageData);
+	if (numOfAliveNeighbours < 2 || numOfAliveNeighbours > 3)
+	  die(i, j, size);
+	if (numOfAliveNeighbours == 3)
+	  live(i, j, size);
+      }
+    }
+  }
+  
+  function live(i, j, size) {
+    context.fillStyle = aliveColor;
+    context.fillRect (i - Math.floor(size/2), j - Math.floor(size/2), size, size);
+  }
+  
+  function die(i, j, size) {
+    context.fillStyle = deadColor;
+    context.fillRect (i - Math.floor(size/2), j - Math.floor(size/2), size, size);
+  }
+
+  // Return the number of alive cell around i,j
+  function neighbours(i, j, size, imageData) {
+    var numOfAlive = 0;
+    numOfAlive += isAlive(imageData, i, j + size);
+    numOfAlive += isAlive(imageData, i, j - size);
+    numOfAlive += isAlive(imageData, i + size, j);
+    numOfAlive += isAlive(imageData, i - size, j);
+    numOfAlive += isAlive(imageData, i + size, j + size);
+    numOfAlive += isAlive(imageData, i - size, j + size);
+    numOfAlive += isAlive(imageData, i + size, j - size);
+    numOfAlive += isAlive(imageData, i - size, j -  size);
+    return numOfAlive;
+  }
+
+  // Check if the cell is alive or not
+  function isAlive(imageData, x, y) { 
+    if (x < 0 || y < 0 || x > imageData.width || y > imageData.height) {
+      return 0;
+    }
+    var offset = x * 4 + y * 4 * imageData.width;
+    r = imageData.data[offset];
+    g = imageData.data[offset+1];
+    b = imageData.data[offset+2];
+    return (r == 0 && g == 0 && b == 0) ? 0 : 1;
+  }
+
+  function canvasSetSize() {
+    canvas.width = 600;
+    canvas.height = 400;
+  }
+
+  // Pattern 1 Infinite Line
   function infiniteLine() {
     var s = Math.floor(size/2);
     var sy = s+size*40;
@@ -65,76 +136,6 @@
     live(sx+size*36,sy,size);
     live(sx+size*37,sy,size);
     live(sx+size*38,sy,size);
-
-
-  }
-
-  function draw(e) {
-    context.fillStyle = aliveColor;
-    var x = (Math.ceil(e.clientX/3)*3) + Math.floor(size/2);
-    var y = (Math.ceil(e.clientY/3)*3) + Math.floor(size/2);
-    context.fillStyle = aliveColor;
-    context.fillRect (x, y, 10*size, 10*size);
-  }
-
-  function nextGeneration() {
-    var width, height, imageData;
-    width = canvas.width;
-    height = canvas.height;
-    imageData = context.getImageData(0, 0, width, height);
-    var numOfAliveNeighbours;
-    for (var i = Math.floor(size/2); i < width; i = i+size){
-      for (var j = Math.floor(size/2); j < height; j = j+size){
-	numOfAliveNeighbours = neighbours(i, j, size, imageData);
-	if (numOfAliveNeighbours < 2 || numOfAliveNeighbours > 3)
-	  die(i, j, size);
-	if (numOfAliveNeighbours == 3)
-	  live(i, j, size);
-      }
-    }
-  }
-  function live(i, j, size) {
-
-
-    context.fillStyle = aliveColor;
-    context.fillRect (i - Math.floor(size/2), j - Math.floor(size/2), size, size);
-  }
-  function die(i, j, size) {
-    context.fillStyle = deadColor;
-    context.fillRect (i - Math.floor(size/2), j - Math.floor(size/2), size, size);
-  }
-
-
-  function neighbours(i, j, size, imageData) {
-    var numOfAlive = 0;
-    numOfAlive += isAlive(imageData, i, j + size);
-    numOfAlive += isAlive(imageData, i, j - size);
-    numOfAlive += isAlive(imageData, i + size, j);
-    numOfAlive += isAlive(imageData, i - size, j);
-    numOfAlive += isAlive(imageData, i + size, j + size);
-    numOfAlive += isAlive(imageData, i - size, j + size);
-    numOfAlive += isAlive(imageData, i + size, j - size);
-    numOfAlive += isAlive(imageData, i - size, j -  size);
-    return numOfAlive;
-  }
-
-  function isAlive(imageData, x, y) { 
-    if (x < 0 || y < 0 || x > imageData.width || y > imageData.height) {
-      return 0;
-    }
-    var offset = x * 4 + y * 4 * imageData.width;
-    
-    r = imageData.data[offset];
-    g = imageData.data[offset+1];
-    b = imageData.data[offset+2];
-    return (r == 0 && g == 0 && b == 0) ? 0 : 1;
-  }
-
-  function canvasSetSize() {
-    //canvas.width = window.innerWidth;
-    //canvas.height = window.innerHeight;
-    canvas.width = 600;
-    canvas.height = 400;
   }
 
   var gameoflife = {
